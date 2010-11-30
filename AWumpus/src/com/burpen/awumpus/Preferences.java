bdabacf727cbe9dac5d5ac49e00097799296743d
burpenator <burpenator@ffeb808f-b40b-e98d-ebaf-3849c77923c8>
2010-11-30 23:14:28 +0000

    Fixed difficulty preference list

--- AWumpus/src/com/burpen/awumpus/Preferences.java
+++ AWumpus/src/com/burpen/awumpus/Preferences.java
@@ -2,16 +2,29 @@ package com.burpen.awumpus;
 
 import com.burpen.awumpus.R;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
+	private ListPreference difficultyPreference;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    addPreferencesFromResource(R.xml.preferences);
+	    difficultyPreference = (ListPreference) getPreferenceScreen().findPreference("difficulty");
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+        // TODO fix this
+        difficultyPreference.setSummary("Currently set to " + sharedPreferences.getString("difficulty", ""));
 	}
 
 }
